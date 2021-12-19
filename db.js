dcfeddb36bfaa7426bbf4bc3fbf895919a0aabdc
Вова Барибін <oknalogia@outlook.com>
2021-12-19 19:06:43 +0200

    was added configuration to sequelize

--- db.js
+++ db.js
@@ -1,5 +1,16 @@
-const { Sequalize } = require('sequalize')
+const { Sequelize } = require('sequelize')
+const dotenv = require('dotenv')
+dotenv.config()
 
-module.exports = new Sequalize(
-  // configuration
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
+
+module.exports = new Sequelize(
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: DB_HOST,
+    port: DB_PORT,
+  }
 )
