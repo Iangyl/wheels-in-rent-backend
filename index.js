3bb6029f237cb9fca2c46bc81efcef7b204363f4
Вова Барибін <oknalogia@outlook.com>
2021-12-19 19:11:57 +0200

    was connected db to application

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
+const sequelize = require('./db')
 
-const express = require('express')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`SERVER WAS STARTED ON PORT ${PORT}`))
+const start = () => {
+  try {
+    await sequelize.authenticate() // connection 
+    await sequelize.sync() // sync db with data shema
+    app.listen(PORT, () => console.log(`SERVER WAS STARTED ON PORT ${PORT}`))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start()
