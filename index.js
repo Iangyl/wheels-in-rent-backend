e529147ba1af05d2eb84179d6b519e0344c403f9
Вова Барибін <oknalogia@outlook.com>
2022-01-08 18:14:59 +0200

    was added cors rules and routes

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
 const sequelize = require('./db')
+const cors = require('cors')
 const models = require('./models/models')
-
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/v1', router)
 
 const start = async () => {
   try {
