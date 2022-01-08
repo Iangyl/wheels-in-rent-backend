5ca5f577d4bd5acb0538b401e798d1a687a85e54
Вова Барибін <oknalogia@outlook.com>
2022-01-08 20:46:43 +0200

    was added new router

---
+++ routes/brandRouter.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const router =  new Router()
+const BrandController = require('./../controllers/brandController')
+
+router.post('/add-brand', BrandController.addBrand)
+
+router.delete('/remove-brand', BrandController.deleteBrand)
+
+router.put('/update-brand', BrandController.updateBrand)
+
+router.get('/brands', BrandController.getAllBrands)
+
+module.exports = router
