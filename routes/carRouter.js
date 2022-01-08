6fc6f7d3d3a1659584dfc01dc72f890430501ff0
Вова Барибін <oknalogia@outlook.com>
2022-01-08 18:37:49 +0200

    was modified car router

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -1,12 +1,15 @@
 const Router = require('express')
 const router =  new Router()
+const CarController = require('./../controllers/carController')
 
-router.get('/', (req, res) => {
+router.get('/', CarController.getCars)
 
-})
+router.get('/car/:id', CarController.getCar)
 
-router.post('/admin/add-car', (req, res) => {
+router.post('/admin/add-car', CarController.newCar)
 
-})
+router.delete('/admin/remove-car', CarController.deleteCar)
+
+router.put('/admin/update-car', CarController.updateCar)
 
 module.exports = router
