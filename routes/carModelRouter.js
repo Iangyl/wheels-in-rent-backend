e9dd0e5f7767292967242d284eff757400858964
Вова Барибін <oknalogia@outlook.com>
2022-01-15 17:19:26 +0200

    was created car model router

---
+++ routes/carModelRouter.js
@@ -0,0 +1,15 @@
+const Router = require('express')
+const router =  new Router()
+const ModelController = require('./../controllers/modelController')
+
+router.get('/models', ModelController.getAllModels)
+
+router.get('/model/:id', ModelController.getModelById)
+
+router.post('/add-model', ModelController.addModel)
+
+router.delete('/remove-model', ModelController.deleteModel)
+
+router.put('/update-model', ModelController.updateModel)
+
+module.exports = router
