e2643ad419cb2bab9fc377401dfd502391c0e520
Вова Барибін <oknalogia@outlook.com>
2022-01-19 00:32:43 +0200

    was created sign-in/sign-up functions and fixed user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,80 @@
 const ApiError = require('./../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { User, Personal } = require('./../models/models')
+const { use } = require('bcrypt/promises')
+
+const generateJWT = (id, email, role) => {
+  return jwt.sign({
+      id,
+      email,
+      role
+    },
+    process.env.PRIVATE_KEY,
+    {
+      expiresIn: '8h'
+    }
+  )
+}
 
 class UserController {
-  async signUp (req, res) {
+  async signUp(req, res, next) {
+    const { name, email, password, tel, birth, role } = req.body
 
+    if (!email || !password || !tel) {
+      next(ApiError.badRequest('Invalid password or email'))
+    }
+
+    const candidate = await User.findOne({
+      where: {
+        email,
+        personalTel: tel
+      }
+    })
+
+    if (candidate) {
+      return next(ApiError.badRequest('The user is already exist!'))
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5)
+    const personal = await Personal.create({ tel, birth })
+    const user = await User.create({
+      name,
+      email,
+      password: hashPassword,
+      role,
+      personalTel: personal.tel
+    })
+
+    const token = generateJWT(user.id, user.email, user.role)
+  
+    return res.json({ message: 'Success!', token })
   }
 
-  async signIn (req, res) {
+  async signIn(req, res, next) {
+    const { email, password } = req.body
+    const user = await User.findOne({
+      where: {
+        email
+      }
+    })
 
+    if (!user) {
+      return next(ApiError.internal("User is not exist!"))
+    }
+
+    const comparePassword = bcrypt.compareSync(password, user.password)
+
+    if (!comparePassword) {
+      return next(ApiError.badRequest("Wrong password!"))
+    }
+
+    const token = generateJWT(user.id, user.email, user.role)
+
+    return res.json({ message: 'Success!', token })
   }
 
-  async check (req, res, next) {
+  async check(req, res, next) {
     const { id } = req.query
     if (!id) {
       return next(ApiError.badRequest('Id not specified!'))
--- models/models.js
+++ models/models.js
@@ -5,8 +5,8 @@ const User = sequelize.define('user', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  role: { type: DataTypes.STRING, allowNull: false },
-  password: { type: DataTypes.INTEGER },
+  role: { type: DataTypes.STRING, defaultValue: 'USER', allowNull: false },
+  password: { type: DataTypes.STRING },
 })
 
 const Personal = sequelize.define('personal', {
